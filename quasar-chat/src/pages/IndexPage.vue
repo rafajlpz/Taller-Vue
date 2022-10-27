b9d7bc88749968fdf4a3888048d9232d07655fba
rafajlz <frilo_94@hotmail.com>
2022-10-27 17:29:43 +0200

    Chat creado, respuesta en tiempo real

--- quasar-chat/src/pages/IndexPage.vue
+++ quasar-chat/src/pages/IndexPage.vue
@@ -6,31 +6,40 @@
   <q-page v-else padding>
     <div class="q-pa-md row justify-center">
       <div style="width: 100%; max-width: 400px">
-        <q-chat-message :text="['hey, how are you?']" sent name="rafajlpz" />
-        <q-chat-message :text="['doing fine, how r you?']" name="xaxa" />
+        <template 
+        v-for="message in messages" 
+        :key="message.id">
+
+          <q-chat-message
+            :text="[message.text]"
+            :sent="message.uid == auth.currentUser.uid"
+            :name="message.displayName"
+          />
+        </template>
       </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
-import { collection, query, onSnapshot } from "firebase/firestore";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import { inject } from "vue";
-import { db } from "src/firebase";
+import { auth, db } from "src/firebase";
+import { ref } from "vue";
 
-const q = query(collection(db, "chats"));
+const q = query(collection(db, "chats"), orderBy('time'));
 const unsubscribe = onSnapshot(q, (snapshot) => {
   snapshot.docChanges().forEach((change) => {
     if (change.type === "added") {
-        console.log("Nuevo chat: ", change.doc.data());
-        messages.value.push ({
-          id: change.doc.id,
-          ...change.doc.data(),
-        })
+      console.log("Nuevo chat: ", change.doc.data());
+      messages.value.push({
+        id: change.doc.id,
+        ...change.doc.data(),
+      });
     }
   });
 });
 
 const userGoogle = inject("userGoogle"); // Traemos la sesion activa
-const messages = ref([])
+const messages = ref([]);
 </script>
