33f6bc32ad669e318717eb36a38ff39bb136a814
rafajlz <frilo_94@hotmail.com>
2022-10-29 08:57:53 +0200

    Inicio logica watchEffect

--- quasar-chat/src/pages/IndexPage.vue
+++ quasar-chat/src/pages/IndexPage.vue
@@ -4,7 +4,7 @@
   </q-page>
 
   <q-page v-else padding>
-    <div class="q-pa-md row justify-center scrollChat"  ref="chatRef">
+    <div class="q-pa-md row justify-center scrollChat" ref="chatRef">
       <div style="width: 100%; max-width: 400px">
         <template v-for="message in messages" :key="message.id">
           <q-chat-message
@@ -24,34 +24,33 @@ import { inject } from "vue";
 import { auth, db } from "src/firebase";
 import { ref, nextTick } from "vue";
 
-const q = query(collection(db, "chats"), orderBy("time"));
-const unsubscribe = onSnapshot(q, (snapshot) => {
-  
-  snapshot.docChanges().forEach(async (change) => {
-    if (change.type === "added") {
-      console.log("Nuevo chat: ", change.doc.data());
-      messages.value.push({
-        id: change.doc.id,
-        ...change.doc.data(),
-      });
-      await nextTick();
-      chatRef.value.scrollTo(0, chatRef.value.scrollHeight);
-
-    }
-    
-  });
-  
-});
 
 const userGoogle = inject("userGoogle"); // Traemos la sesion activa
 const messages = ref([]);
 const chatRef = ref(null);
+
+
+if (userGoogle.value) {
+  const q = query(collection(db, "chats"), orderBy("time"));
+  const unsubscribe = onSnapshot(q, (snapshot) => {
+    snapshot.docChanges().forEach(async (change) => {
+      if (change.type === "added") {
+        console.log("Nuevo chat: ", change.doc.data());
+        messages.value.push({
+          id: change.doc.id,
+          ...change.doc.data(),
+        });
+        await nextTick();
+        chatRef.value.scrollTo(0, chatRef.value.scrollHeight);
+      }
+    });
+  });
+}
 </script>
 
 <style>
-.scrollChat{
+.scrollChat {
   height: calc(100vh - (60px + 60px));
   overflow-y: scroll;
 }
-
 </style>
